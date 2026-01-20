b0fbdd460010567e12e033789cfd7e8a8053e6e1
PRINCE BHADANIA <princebhadania007@gmail.com>
2026-01-20 10:01:06 +0530

    Add files via upload

---
+++ 20_Detect_Pattern_M.cpp
@@ -0,0 +1,52 @@
+// Given an array of positive integers arr, find a pattern of length m that is repeated k or more times.
+
+// A pattern is a subarray (consecutive sub-sequence) that consists of one or more values, repeated multiple times consecutively without overlapping. A pattern is defined by its length and the number of repetitions.
+
+// Return true if there exists a pattern of length m that is repeated k or more times, otherwise return false.
+
+ 
+
+// Example 1:
+
+// Input: arr = [1,2,4,4,4,4], m = 1, k = 3
+// Output: true
+// Explanation: The pattern (4) of length 1 is repeated 4 consecutive times. Notice that pattern can be repeated k or more times but not less.
+// Example 2:
+
+// Input: arr = [1,2,1,2,1,1,1,3], m = 2, k = 2
+// Output: true
+// Explanation: The pattern (1,2) of length 2 is repeated 2 consecutive times. Another valid pattern (2,1) is also repeated 2 times.
+// Example 3:
+
+// Input: arr = [1,2,1,2,1,3], m = 2, k = 3
+// Output: false
+// Explanation: The pattern (1,2) is of length 2 but is repeated only 2 times. There is no pattern of length 2 that is repeated 3 or more times.
+
+
+
+
+
+
+
+// My code and also claude helped
+
+class Solution {
+public:
+    bool containsPattern(vector<int>& arr, int m, int k) {
+        int n = arr.size();
+        int count = 0;
+        
+        for (int i = 0; i + m < n; i++) {
+            if (arr[i] == arr[i + m]) {
+                count++;
+                if (count == (k - 1) * m) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        
+        return false;
+    }
+};
