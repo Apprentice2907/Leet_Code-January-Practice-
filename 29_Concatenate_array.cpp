1a577960b9f72188ffb3e22d727d2923b08b2e78
PRINCE BHADANIA <princebhadania007@gmail.com>
2026-01-29 14:57:47 +0530

    Add files via upload

---
+++ 29_Concatenate_array.cpp
@@ -0,0 +1,61 @@
+// You are given an array of distinct integers arr and an array of integer arrays pieces, where the integers in pieces are distinct. Your goal is to form arr by concatenating the arrays in pieces in any order. However, you are not allowed to reorder the integers in each array pieces[i].
+
+// Return true if it is possible to form the array arr from pieces. Otherwise, return false.
+
+
+// Example 1:
+
+// Input: arr = [15,88], pieces = [[88],[15]]
+// Output: true
+// Explanation: Concatenate [15] then [88]
+// Example 2:
+
+// Input: arr = [49,18,16], pieces = [[16,18,49]]
+// Output: false
+// Explanation: Even though the numbers match, we cannot reorder pieces[0].
+// Example 3:
+
+// Input: arr = [91,4,64,78], pieces = [[78],[4,64],[91]]
+// Output: true
+// Explanation: Concatenate [91] then [4,64] then [78]
+ 
+
+
+
+
+
+
+
+
+// ChatGPT coded but understood and also optimal too 
+
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        int i = 0;
+
+        while (i < arr.size()) {
+            bool found = false;
+
+            for (int p = 0; p < pieces.size(); p++) {
+                if (pieces[p][0] == arr[i]) {
+                    found = true;
+
+                    for (int j = 0; j < pieces[p].size(); j++) {
+                        if (i >= arr.size() || arr[i] != pieces[p][j]) {
+                            return false;
+                        }
+                        i++;
+                    }
+                    break; 
+                }
+            }
+
+            if (!found) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
