375264ba6b4d9e279910590a57446bd44e6daa18
PRINCE BHADANIA <princebhadania007@gmail.com>
2026-01-31 23:07:49 +0530

    Add files via upload

---
+++ 31_Maximum_Repeating_Substring.cpp
@@ -0,0 +1,92 @@
+// For a string sequence, a string word is k-repeating if word concatenated k times is a substring of sequence. The word's maximum k-repeating value is the highest value k where word is k-repeating in sequence. If word is not a substring of sequence, word's maximum k-repeating value is 0.
+
+// Given strings sequence and word, return the maximum k-repeating value of word in sequence.
+
+// Example 1:
+
+// Input: sequence = "ababc", word = "ab"
+// Output: 2
+// Explanation: "abab" is a substring in "ababc".
+// Example 2:
+
+// Input: sequence = "ababc", word = "ba"
+// Output: 1
+// Explanation: "ba" is a substring in "ababc". "baba" is not a substring in "ababc".
+// Example 3:
+
+// Input: sequence = "ababc", word = "ac"
+// Output: 0
+// Explanation: "ac" is not a substring in "ababc".
+
+
+
+
+
+// My approach 
+
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        string current = word;
+        int count = 0;
+
+        while (sequence.find(current) != string::npos) {
+            count++;
+            current += word;
+        }
+
+        return count;
+    }
+};
+
+
+
+
+
+
+
+
+//leetcode Optimal approach 
+
+class Solution {
+public:
+
+    int maxRepeating(string sequence, string word) {
+        int n=sequence.size();
+        int count =0;
+        int k=word.size();
+        int ans =0;
+        int i=0;
+        // while(i<n){
+        //     if(i+k<=n &&  sequence.substr(i,k)==word){
+        //             count++;
+        //             i+=k;
+        //             ans=max(count , ans );
+        //         }
+        //     else{
+                    
+        //             if(count>0){
+        //                 i=i-k+1;
+        //                 // while(sequence.substr(i,k)==word)
+        //             }
+        //             else i++;
+                    
+        //             ans=max(count , ans );
+        //             count =0;
+        //         }
+            
+        //     }
+            
+            int c=0;
+            string w=word;
+            while(w.size()<=sequence.size()){
+                if(sequence.find(w)!=string::npos){
+                    c++;
+                }
+                
+             
+                w+=word;
+            }
+        return c;
+    }
+};
